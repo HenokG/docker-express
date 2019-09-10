cd9870ec62a44676641bea93211606808d9b923b
HenokG <hhenok1987@gmail.com>
2019-09-10 17:23:49 +0300

    introduce containerized workflow using docker and update http response

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) =>
+  res.send(
+    "Hello, This app was written with express(NodeJS) and deployed to Heroku using docker for containerized workflow."
+  )
+);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
